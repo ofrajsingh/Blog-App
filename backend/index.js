69e0499bdc4f8c0eadc363bfbbae77e136f204c9
ofrajsingh <rajsingh00seven@gmail.com>
2023-09-26 00:58:34 +0530

    asdf

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,57 @@
-const express= require('express');
-const app= express();
-const mongoose= require('mongoose');
-const dotenv= require('dotenv');
-const cors= require('cors');
-const multer= require('multer');
-const path= require('path');
-const cookieParser= require('cookie-parser');
-const authRoute= require('./routes/auth');
-const userRoute= require('./routes/users');
-const postRoute= require('./routes/posts');
-const commentRoute= require('./routes/comments');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
 
-const connectDB= async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("database is connected successfully");
-    }catch(err){
-        console.log(err);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database is connected successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 // middlewares
 dotenv.config();
 app.use(express.json());
-app.use('/images',express.static(path.join(__dirname,'/images')))
-app.use(cors({origin: 'http://localhost:5173',credentials:true}));
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
-app.use('/api/auth',authRoute);
-app.use('/api/users',userRoute);
-app.use('/api/posts',postRoute);
-app.use('/api/comments',commentRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
 //image upload
-const storage=multer.diskStorage({
-    destination:(req,file,fn)=>{
-        fn(null,'images')
-    },
-    filename:(req,file,fn)=>{
-        fn(null,req.body.img)
-    }
-})
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+  },
+});
 
-const upload= multer({storage:storage});
-app.post('/api/upload',upload.single('file'),(req,res)=>{
-    // console.log(req.body);
-    res.status(200).json("image has been uploaded succesfully!");
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  // console.log(req.body);
+  res.status(200).json("image has been uploaded succesfully!");
+});
 
-const PORT= process.env.PORT;
-app.listen(PORT,()=>{
-    connectDB();
-    console.log("App is running on port "+PORT);
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  connectDB();
+  console.log("App is running on port " + PORT);
+});
