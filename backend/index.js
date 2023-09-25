ff9a895dd0b8f06352b1623d53d3aecf4f7a1ea0
ofrajsingh <rajsingh00seven@gmail.com>
2023-09-26 01:44:17 +0530

    dead

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,10 @@ const connectDB = async () => {
     console.log(err);
   }
 };
-app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*" });
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header({ "Access-Control-Allow-Origin": "*" });
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -31,9 +31,10 @@ app.get("/", (req, res) => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(
-  cors({ origin: "https://blog-app-c2th.onrender.com", credentials: true })
-);
+// app.use(
+//   cors({ origin: "https://blog-app-c2th.onrender.com", credentials: true })
+// );
+app.use(cors());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
