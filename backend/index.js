3e3049a5a80d847db3a2f07fb6f849db59268ba4
ofrajsingh <rajsingh00seven@gmail.com>
2023-09-27 02:34:57 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ const connectDB = async () => {
   }
 };
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
