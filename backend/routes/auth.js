cff069665f21c589514e4c35880d8f0d6b5c01db
ofrajsingh <rajsingh00seven@gmail.com>
2023-09-30 16:47:50 +0530

    test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -35,7 +35,8 @@ router.post("/login", async (req, res) => {
     }
     const token= jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn: '3d'});
     const {password,...info}= user._doc;
-    res.cookie('token',token).status(200).json(info);
+    res.cookie('token',token,{httpOnly : false}).status(200).json(info);
+    // res.status(200).json(token);
   } catch (err) {
     res.status(500).json(err);
   }
